082a6ac1c649ff4b72e6875a48b8c5af10608053
mhsRMilne <48797365+mhsRMilne@users.noreply.github.com>
2019-04-08 08:38:53 -0400

    Add files via upload

---
+++ movement & if/sketch.js
@@ -0,0 +1,71 @@
+/*
+Movement & If Statements
+Created by Ryan Milne
+April 4th, 2019
+*/
+
+//Global variables
+let x=100;
+let y=100;
+
+let xSpeed = 7;
+let ySpeed = 7;
+
+let rectWidth = 50;
+let rectHeight = 50;
+
+
+
+function setup() {
+  // put setup code here
+  createCanvas(1000,800,WEBGL);
+
+
+}
+
+function draw() {
+  // put drawing code here
+
+//change the background
+
+background(255);
+
+//rectangle width & height
+
+
+
+rotateX(frameCount * 0.1);
+rotateZ(frameCount * 0.1);
+cylinder(50,250);
+
+//draw rectangle
+
+  rect(x,y,rectWidth,rectHeight);
+  rect(x+100,y+50,rectWidth,rectHeight);
+
+//move rectangle
+  x = x+xSpeed;
+  y = y+ySpeed;
+
+//if reaches bottom or top, change direction
+  if (y+rectHeight>=height || y<=0) {
+  ySpeed = ySpeed*(-1);
+
+  fill(random(0,255),random(0,255),random(0,255));
+//  background(random(0,255),random(0,255),random(0,255));
+
+  }
+
+//if reaches right or left, change direction
+else if (x+rectWidth>=width || x<=0) {
+  xSpeed = xSpeed*(-1);
+
+  fill(random(0,255),random(0,255),random(0,255));
+  // background(random(0,255),random(0,255),random(0,255));
+
+  }
+
+
+
+
+}
