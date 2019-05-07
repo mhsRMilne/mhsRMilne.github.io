cd312c5685697a6195de5d0baaed09e87d2deafc
mhsRMilne <48797365+mhsRMilne@users.noreply.github.com>
2019-05-07 10:49:43 -0400

    Add files via upload

--- looping structures/sketch.js
+++ looping structures/sketch.js
@@ -23,14 +23,14 @@ var x = 0
 for (var x = 0; x <= width; x = x + size) {
 // larger circles
   noStroke();
-  fill(0,random(255),0);
+  fill(random(255),0,random(255));
   ellipse(x,random(windowHeight),size,size);
 // random symbols
   textSize(25);
   fill(random(255));
   text(char(random(255)),random(windowWidth),random(windowHeight));
 // smaller circles
-  fill(random(255),0,random(255));
+  fill(0,random(255),0);
   ellipse(x,random(windowHeight),size-size/2,size-size/2);
 
 
