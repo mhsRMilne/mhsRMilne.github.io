3984db7d6e9cc99c6c7a4c190b6cca57b442377e
mhsRMilne <48797365+mhsRMilne@users.noreply.github.com>
2019-05-08 08:45:38 -0400

    Add files via upload

--- snowman drawing/sketch.js
+++ snowman drawing/sketch.js
@@ -1,27 +1,31 @@
-//snowman drawing, ryan milne, march 7th 2019
+/*
+Snowman Drawing
+Ryan Milne
+March 7th, 2019
+*/
 
 function setup() {
   // put setup code here
   createCanvas(400,400);
-}
+
+} // end setup
 
 function draw() {
   // put drawing code here
   background(0, 255, 255);
 
-  // The ground
+  // the ground
   fill(0, 255, 102);
   rect(0, 300, 400, 100);
 
-  // The sun
+  // the sun
   fill(255, 234, 0);
   ellipse(80, 64, 100, 100);
 
-  // The snowman
+  // the snowman
   fill(255, 255, 255);
   ellipse(200, 300, 150, 150);
   ellipse(200, 200, 100, 100);
   ellipse(200, 120, 75, 75);
 
-
-}
+} // end draw
