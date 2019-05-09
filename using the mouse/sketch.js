195f8166a19251bc0eae0526e4aa714937422feb
mhsRMilne <48797365+mhsRMilne@users.noreply.github.com>
2019-05-09 10:31:54 -0400

    Add files via upload

--- using the mouse/sketch.js
+++ using the mouse/sketch.js
@@ -1,39 +1,37 @@
 /*
 Using the Mouse
-Created by Ryan Milne
+Ryan Milne
 April 10th, 2019
 */
-
 
 let x = 1
 
 function setup() {
   // put setup code here
-createCanvas(windowWidth,windowHeight);
+  // create the canvas
+  createCanvas(windowWidth,windowHeight);
 
-}
+} // end setup
 
 function draw() {
   // put drawing code here
-
-// turn into squares
+  // turn into squares
   if (mouseIsPressed) {
-  if(x<=width) {
 
-     rect(mouseX,mouseY,75,75);
-   }
-   }
+    if(x<=width) {
+      rect(mouseX,mouseY,75,75);
+    }
 
-}
+  }
 
+} // end draw
 
-
-// make circles
+// make ellipses
 function mouseMoved() {
 
- if(x<=width) {
-ellipse(mouseX,mouseY,100,100);
-fill(random(0,255),random(0,255),random(0,255));
- }
+  if(x<=width) {
+    ellipse(mouseX,mouseY,100,100);
+    fill(random(0,255),random(0,255),random(0,255));
+  }
 
-}
+} // end mouseMoved
