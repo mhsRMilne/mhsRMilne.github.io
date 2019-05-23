2c1035781372055d7ee6d2bc7a6c8f2b218f55ea
mhsRMilne <48797365+mhsRMilne@users.noreply.github.com>
2019-05-23 10:20:55 -0400

    Update sketch.js

--- looping structures/sketch.js
+++ looping structures/sketch.js
@@ -3,38 +3,35 @@ Looping Structures
 Created by Ryan Milne
 May 7th, 2019
 */
+
 let x = 0
 let y = 100
 let size = 40
-
 
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
   background(255);
-
-}
+  
+} // end setup
 
 function draw() {
   // put drawing code here
 
-var x = 0
+  var x = 0
 
-for (var x = 0; x <= width; x = x + size) {
-// larger circles
-  noStroke();
-  fill(random(255),0,random(255));
-  ellipse(x,random(windowHeight),size,size);
-// random symbols
-  textSize(25);
-  fill(random(255));
-  text(char(random(255)),random(windowWidth),random(windowHeight));
-// smaller circles
-  fill(0,random(255),0);
-  ellipse(x,random(windowHeight),size-size/2,size-size/2);
-
-
-
-}
-
-}
+  for (var x = 0; x <= width; x = x + size) {
+    // larger circles
+    noStroke();
+    fill(random(255),0,random(255));
+    ellipse(x,random(windowHeight),size,size);
+    // random symbols
+    textSize(25);
+    fill(random(255));
+    text(char(random(255)),random(windowWidth),random(windowHeight));
+    // smaller circles
+    fill(0,random(255),0);
+    ellipse(x,random(windowHeight),size-size/2,size-size/2);
+  }
+  
+} // end draw
