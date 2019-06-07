ab51099e1e379cfc6be4ff630cd47c9b6e46f229
mhsRMilne <48797365+mhsRMilne@users.noreply.github.com>
2019-06-07 10:26:36 -0400

    Add files via upload

---
+++ RyanMilne_Summative/sketch.js
@@ -0,0 +1,15 @@
+/*
+SUMMATIVE ASSIGNMENT
+Created by Ryan Milne
+June 7th, 2019
+*/
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+} // end setup
+
+function draw() {
+  // put drawing code here
+
+} // end draw
